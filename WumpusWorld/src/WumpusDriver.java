42383b012784cc5ca931bddfed44e8a393f79bf2
Weston Smith <weston@Westons-MacBook-Pro.local>
2017-10-09 15:51:25 -0500

    added functionality to Map class

--- WumpusWorld/src/WumpusDriver.java
+++ WumpusWorld/src/WumpusDriver.java
@@ -3,7 +3,18 @@ Main Driver for the Wumpus World Program
 **********************************************/
 
 public class WumpusDriver{
-  public static void main(){
+  public static void main(String[] args){
       System.out.println("The Wumpus Says COME TO MEEEEEE");
+
+      coordinate s = new coordinate(0,0);
+      coordinate w = new coordinate(9,6);
+      coordinate sup = new coordinate(1,5);
+      coordinate[] p = new coordinate[3];
+      p[0] = new coordinate(4,3);
+      p[1] = new coordinate(6,0);
+      p[2] = new coordinate(2,3);
+      coordinate g = new coordinate(9,9);
+      Map m = new Map(s, w, sup, g, p);
+      m.print();
   }
 }
